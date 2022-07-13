aa696960cef703b05ba5f0e08a129ac74fcf21fe
Sachin-chamoli <79299103+Sachin-chamoli@users.noreply.github.com>
2022-07-13 13:01:08 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const four = document.getElementById("4");
+
+one.addEventListener("click", (e)=>{
+    one.style. backgroundColor=" rgb(39, 56, 187)";
+    one.style.color=" white";
+    one.style.transform= "scale(1.3)";
+})
+two.addEventListener("click", (e)=>{
+    two.style. backgroundColor=" rgb(39, 56, 187)";
+    two.style.color=" white";
+    two.style.transform= "scale(1.3)";
+})
+three.addEventListener("click", (e)=>{
+    three.style. backgroundColor=" rgb(39, 56, 187)";
+    three.style.color=" white";
+    three.style.transform= "scale(1.3)";
+})
+four.addEventListener("click", (e)=>{
+    four.style. backgroundColor=" rgb(39, 56, 187)";
+    four.style.color=" white";
+    four.style.transform= "scale(1.3)";
+})
